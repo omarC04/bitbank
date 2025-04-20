016072d1570a873edc7a9d5636e29721064b08c0
omarC04 <ohhmarcarmona00@gmail.com>
2025-04-19 17:48:24 -0700

    Set up server-side directory structure

---
+++ server/src/main/java/com/bytecorporation/bitbank/BitbankApplication.java
@@ -0,0 +1,13 @@
+package com.bytecorporation.bitbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitbankApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/bytecorporation/bitbank/BitbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytecorporation.bitbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
